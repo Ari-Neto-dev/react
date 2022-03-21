fef11bd6b7dc3e7e8067e3b4dced7e8587827ff1
Ari Azevedo <azevedo.consultor@hotmail.com>
2022-03-21 20:47:15 -0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+/*import React from 'react';*/
+import {render} from 'react-dom';
+import { App } from './App'
+
+render(<App />, document.getElementById('root'))
+
+
+
+/*function App() {
+  return <h1>Hello World</h1>
+}*/
+
+
+
+/*const user = { name:'ARI', }
+console.log(user.address?.street)*/
