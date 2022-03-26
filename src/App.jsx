fc5a9ea91e97fa89b52d62af24d41146acfcd1e7
Ari Azevedo <azevedo.consultor@hotmail.com>
2022-03-26 17:33:46 -0300

    finaliznado a estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { RepositoryList } from './components/RepositoryList';
-import { Counter } from './Counter';
+
 import './styles/global.scss';
 
 export function App() {
     return (
-    <>
        <RepositoryList />
-       <Counter/>
-    </>
+       
+    
     )
 
 }
